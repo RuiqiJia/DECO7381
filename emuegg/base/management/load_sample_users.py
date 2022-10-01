3b56674f251bf621b4efbf202c5af12e28ee9154
JasMq <91511730+JasMq@users.noreply.github.com>
2022-10-01 13:49:29 +1000

    load sample user
    load_sample_user

--- emuegg/base/management/load_sample_users.py
+++ emuegg/base/management/load_sample_users.py
@@ -11,4 +11,28 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         # remove the existing user data?
+        User.objects.all().delete()
+        path = kwargs['path']
+        user_df = pd.read_csv(path)
+
+        for index, row in user_df.iterrows():
+            email = row["email"]
+            username = row["username"]
+            major = row["major"]
+            country = row["country"]
+            course = row["course"]
+
+            # populate User object for each row
+            user = User(email = email,
+                        Country = country,
+                        Major = major,
+                        Course = course)
+
+        # save user object
+        user.save()
+        print(f"User: {email}, {username} saved...")
+
+
+# Use following command to load sample user to database
+# python manage.py load_sample_users --path sample_user.csv
 
